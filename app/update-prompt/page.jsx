2e34970e3eaeff555f6c5f9aea1686ff744cc7a8
Waroon <waroon1999@hotmail.com>
2024-06-23 22:22:38 +0700

    fix update-prompt

--- app/update-prompt/page.jsx
+++ app/update-prompt/page.jsx
@@ -1,7 +1,6 @@
 "use client";
 
 import { useState, useEffect } from "react";
-import { useSession } from "next-auth/react";
 import { useRouter, useSearchParams } from "next/navigation";
 
 import Form from "@components/Form";
